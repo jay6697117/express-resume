bc277c3aca2de8ed0f1ef07ca96dded7194e9b21
steve zhang <871506263@qq.com>
2022-02-09 23:44:08 +0800

    提交

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 // 导入 http 模块
 const http = require('http');
-// console.log(`http:\n`, http)
 
 // 指定服务器的主机名 hostname 和端口号 port
 const hostname = 'localhost';
@@ -9,10 +8,9 @@ const port = 3000;
 
 // 用 http.createServer 创建 HTTP 服务器，参数为一个回调函数，接受一个请求对象 req 和响应对象 res，并在回调函数中写入响应内容（状态码 200，类型为 HTML 文档，内容为 Hello World）
 const server = http.createServer((req, res) => {
-  console.log(`req:\n`, req)
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World 001</h1><h2>Hello World 002</h2>');
+  res.statusCode = 200; //手动指定 HTTP 状态码
+  res.setHeader('Content-Type', 'text/html'); //手动指定 HTTP 头部字段
+  res.end('<h1>Hello World 00111</h1><h2>Hello World 00222</h2>'); //返回内容
 });
 
 // 在指定的端口开启服务器
