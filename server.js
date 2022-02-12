c3ab423683dd8b1dd62f266216bc4f34c3756a98
steve zhang <871506263@qq.com>
2022-02-12 22:06:45 +0800

    提交

--- server.js
+++ server.js
@@ -1,19 +1,39 @@
-// 导入 http 模块
-const http = require('http');
-
 // 指定服务器的主机名 hostname 和端口号 port
 const hostname = 'localhost';
-// const hostname = '127.0.0.1';
 const port = 3000;
 
-// 用 http.createServer 创建 HTTP 服务器，参数为一个回调函数，接受一个请求对象 req 和响应对象 res，并在回调函数中写入响应内容（状态码 200，类型为 HTML 文档，内容为 Hello World）
-const server = http.createServer((req, res) => {
-  res.statusCode = 200; //手动指定 HTTP 状态码
-  res.setHeader('Content-Type', 'text/html'); //手动指定 HTTP 头部字段
-  res.end('<h1>Hello World 00111</h1><h2>Hello World 00222</h2>'); //返回内容
+//创建一个 Express 服务器对象
+const express = require('express');
+const app = new express();
+
+let indexMiddlewareStr;
+let loggingMiddlewareStr;
+
+const indexMiddleware = (req, res, next) => {
+  const time = new Date();
+  indexMiddlewareStr = `[${time.toLocaleString()}]\n: ${req.method} ${req.url}`;
+  next();
+};
+const loggingMiddleware = (req, res, next) => {
+  const time = new Date();
+  loggingMiddlewareStr = `[${time.toLocaleString()}]\n: ${req.method} ${req.url}`;
+  next();
+};
+
+app.use(indexMiddleware);
+app.use(loggingMiddleware);
+
+// 定义了主页 / index的路由
+app.get('/', indexMiddleware, (req, res) => {
+  res.send(`indexMiddleware: &nbsp;&nbsp; ${indexMiddlewareStr}`);
 });
 
-// 在指定的端口开启服务器
-server.listen(port, () => {
+// 定义了主页 / loggingMiddleware的路由
+app.get('/loggingMiddleware', loggingMiddleware, (req, res) => {
+  res.send(`loggingMiddleware: &nbsp;&nbsp; ${loggingMiddlewareStr}`);
+});
+
+// 调用 listen 方法开启服务器
+app.listen(port, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
