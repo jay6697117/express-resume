a37fd9d699bc94d4ee2166f45012d3edbbe52d52
steve zhang <871506263@qq.com>
2022-02-12 22:25:16 +0800

    提交

--- server.js
+++ server.js
@@ -5,32 +5,36 @@ const port = 3000;
 //创建一个 Express 服务器对象
 const express = require('express');
 const app = new express();
+app.set('views', 'views');
+app.set('view engine', 'hbs');
 
 let indexMiddlewareStr;
-let loggingMiddlewareStr;
+let contactMiddlewareStr;
 
 const indexMiddleware = (req, res, next) => {
   const time = new Date();
   indexMiddlewareStr = `[${time.toLocaleString()}]\n: ${req.method} ${req.url}`;
   next();
 };
-const loggingMiddleware = (req, res, next) => {
+const contactMiddleware = (req, res, next) => {
   const time = new Date();
-  loggingMiddlewareStr = `[${time.toLocaleString()}]\n: ${req.method} ${req.url}`;
+  contactMiddlewareStr = `[${time.toLocaleString()}]\n: ${req.method} ${req.url}`;
   next();
 };
 
 app.use(indexMiddleware);
-app.use(loggingMiddleware);
+app.use(contactMiddleware);
 
 // 定义了主页 / index的路由
 app.get('/', indexMiddleware, (req, res) => {
-  res.send(`indexMiddleware: &nbsp;&nbsp; ${indexMiddlewareStr}`);
+  console.log('index日志输出:', `indexMiddleware: &nbsp;&nbsp; ${indexMiddlewareStr}`);
+  res.render('index');
 });
 
 // 定义了主页 / loggingMiddleware的路由
-app.get('/loggingMiddleware', loggingMiddleware, (req, res) => {
-  res.send(`loggingMiddleware: &nbsp;&nbsp; ${loggingMiddlewareStr}`);
+app.get('/contact', contactMiddleware, (req, res) => {
+  console.log('contact日志输出:', `contactMiddleware: &nbsp;&nbsp; ${contactMiddlewareStr}`);
+  res.render('contact');
 });
 
 // 调用 listen 方法开启服务器
